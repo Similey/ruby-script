1d4bf7fdd9703fa5fc0e9ceaec8acf0cf6a33272
Blewitt, Jonathan <blewitt.3@gmail.com>
2019-06-20 17:19:28 -0400

    Adding combination function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// import * as helpers from './helpers'
+const helpers = require('./helpers');
+
 class Collection extends Array {
     constructor(array) {
         // call the constructor of the Array class
@@ -18,13 +21,31 @@ class Collection extends Array {
         }
     }
 
-    combination(int) {}
+    combination(int) {
+        let allArray = [];
+
+        let combos = (fullArray, fullLen, pushBack, start, curLen) => {
+            // pushBack stack
+            if (pushBack.length === fullLen)
+                allArray.push(pushBack.slice(0,fullLen));
+
+            if (curLen < 0) return;
+            for (let i=start; i<fullArray.length; i++) {
+                pushBack.push(fullArray[i]);
+                combos(fullArray, fullLen, pushBack, i+1, curLen-1);
+                pushBack.pop();
+            }
+        };
+
+        combos(this, int, [], 0, int);
+        return allArray
+    }
 
 }
 
 let collection = new Collection([1, 2, 3, 4]);
-collection.combination(2);
-console.log(collection);
+a = collection.combination(3);
+console.log(a);
 
 
 // module.exports = {
